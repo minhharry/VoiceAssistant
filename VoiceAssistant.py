0d05408a3c424ce055b1e5bba090b1ead44ad4ba
minhharry <tbminh@gmail.com>
2025-04-24 10:38:26 +0700

    .

--- VoiceAssistant.py
+++ VoiceAssistant.py
@@ -248,21 +248,60 @@ class VoiceAssistant:
         print("Speech Recognition stopped.")
 
 if __name__ == "__main__":
+    from Adafruit_IO import MQTTClient
+    import sys
+    AIO_USERNAME = "example"
+    AIO_KEY      = "example"
+    AIO_FEED_ID = ["BBC-LED", "BBC-FAN"]
+    def  connected(client):
+        print("Ket noi thanh cong...")
+        for v in AIO_FEED_ID:
+            client.subscribe(v)
+
+    def  subscribe(client , userdata , mid , granted_qos):
+        print("Subscribe thanh cong...")
+
+    def  disconnected(client):
+        print("Ngat ket noi...")
+        sys.exit (1)
+
+    def message(client, feed_id, payload):
+        pass
+
+
+    client = MQTTClient(AIO_USERNAME , AIO_KEY)
+    client.on_connect = connected
+    client.on_disconnect = disconnected
+    client.on_message = message
+    client.on_subscribe = subscribe
+    client.connect()
+    client.loop_background()
+
+
     def turn_on_light():
         # Push data lên AdaFruitIO
         print("===...Turning on the light...===")
+        client.publish("BBC-LED", "1")
     
     def turn_off_light():
         # Push data lên AdaFruitIO
         print("===...Turning off the light...===")
+        client.publish("BBC-LED", "0")
     
     def turn_on_fan():
         # Push data lên AdaFruitIO
         print("===...Turning on the fan...===")
+        client.publish("BBC-FAN", "1")
     
     def turn_off_fan():
         # Push data lên AdaFruitIO
         print("===...Turning off the fan...===")
+        client.publish("BBC-FAN", "0")
+
+    def action_factory(name: str, description: str, vietnamese_description: str, keyword):
+        def action_func():
+            print(f"Action: {name}")
+        return Action(name, description, vietnamese_description, keyword, action_func)
 
     action_lst = [
         Action("turn_on_light", "Turn on the light", "bật đèn", "bật đèn", turn_on_light),
